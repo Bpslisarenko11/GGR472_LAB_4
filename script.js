3f6e31817f4e1f78c1c2fffc10ff8f077dfe491d
Bpslisarenko11 <157765674+Bpslisarenko11@users.noreply.github.com>
2024-03-18 18:35:26 -0600

    final updates2

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-/*--------------------------------------------------------------------
-GGR472 LAB 4: Incorporating GIS Analysis into web maps using Turf.js 
---------------------------------------------------------------------*/
-
-/*--------------------------------------------------------------------
-Step 1: INITIALIZE MAP
---------------------------------------------------------------------*/
 // Access Token for map style
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3BibGlzYXJlbmtvMTIiLCJhIjoiY2xzMjlodmljMGthcjJrbXRibnRwZ2d3eCJ9.gxylQolcBDuJTH_WfI6MrA';
 
@@ -23,34 +16,17 @@ map.addControl(new mapboxgl.NavigationControl(), "top-left");
 map.addControl(new mapboxgl.FullscreenControl(), "top-left");
 
 
-
-/*--------------------------------------------------------------------
-Step 2: VIEW GEOJSON POINT DATA ON MAP
---------------------------------------------------------------------*/
-//HINT: Create an empty variable
-//      Use the fetch method to access the GeoJSON from your online repository
-//      Convert the response to JSON format and then store the response in your new variable
-
 // New empty variable
 let cycle;
 
 // Use fetch method to access URL for GeoJSON and add it to variable
-fetch("https://raw.githubusercontent.com/smith-lg/ggr472-lab4/main/data/pedcyc_collision_06-21.geojson")
+fetch("https://bpslisarenko11.github.io/GGR472_LAB_4/data/pedcyc_collision_06-21.geojson")
     .then(response => response.json())
     .then(response => {
         console.log(response);
         cycle = response;
     })
 
-
-
-/*--------------------------------------------------------------------
-    Step 3: CREATE BOUNDING BOX AND HEXGRID
---------------------------------------------------------------------*/
-//HINT: All code to create and view the hexgrid will go inside a map load event handler
-//      First create a bounding box around the collision point data then store as a feature collection variable
-//      Access and store the bounding box coordinates as an array variable
-//      Use bounding box coordinates as argument in the turf hexgrid function
 
 // load map
 map.on('load',() => {
@@ -139,7 +115,7 @@ map.on('load',() => {
     //Add the collision points from the URL source
     map.addSource("collision_points", {
         type: "geojson",
-        data: 'https://raw.githubusercontent.com/bpslisarenko11/GGR472_LAB_4/main/data/pedcyc_collision_06-21.geojson', // Link to GeoJSON link in GitHub
+        data: 'https://bpslisarenko11.github.io/GGR472_LAB_4/data/pedcyc_collision_06-21.geojson', // Link to GeoJSON link in GitHub
     
     });
 
@@ -280,25 +256,5 @@ document.getElementById("collision-type-filter").addEventListener('click',(e) =>
         );
     }
 })
-
-
-
-/*--------------------------------------------------------------------
-Step 4: AGGREGATE COLLISIONS BY HEXGRID
---------------------------------------------------------------------*/
-//HINT: Use Turf collect function to collect all '_id' properties from the collision points data for each heaxagon
-//      View the collect output in the console. Where there are no intersecting points in polygons, arrays will be empty
-
-
-
-// /*--------------------------------------------------------------------
-// Step 5: FINALIZE YOUR WEB MAP
-// --------------------------------------------------------------------*/
-//HINT: Think about the display of your data and usability of your web map.
-//      Update the addlayer paint properties for your hexgrid using:
-//        - an expression
-//        - The COUNT attribute
-//        - The maximum number of collisions found in a hexagon
-//      Add a legend and additional functionality including pop-up windows
 
 
